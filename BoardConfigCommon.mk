b85b6572fb050eaf579a97018d6b178c5c13461e
intervigillium <intervigil@gmail.com>
2017-06-04 22:06:23 +0100

    hlte-common: Allow device to skip the sensor-permission request

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,9 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 -include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/samsung/hlte-common/sepolicy
 
+# Sensors
+TARGET_NO_SENSOR_PERMISSION_CHECK := true
+
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
 BOARD_WLAN_DEVICE := bcmdhd
