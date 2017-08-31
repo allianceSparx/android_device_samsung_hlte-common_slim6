89e2ae4cf915970d84b52693f964c848aa063824
haggertk <haggertk@lineageos.org>
2017-08-31 17:32:37 +0100

    Squashed commit of overlay-based GPS config * Cherry-picked from klte-common
      klte-common: Move to commonized, overlay-based GPS config
    
      * This is one main step toward future consolidating of some klte*
        builds
      * Approach taken from Nexus/Pixel devices, values based on
        previously functional variant gps.conf files combined with
        LPP_PROFILE tuning by carrier from marlin
    
      Change-Id: I9a66eb156df69340d35c139d20f835fa8693a716
    
      klte-common: fw/b overlay: Change GPS capabilities bitmask
    
      * Disable MSB. Quicker and stronger lock. Winning.
      * All regions/carriers are now (GEOFENCE|ON_DEMAND_TIME|SCHEDULING)
      * CDMA carriers were already set to this value
    
      Change-Id: I2b060a11795a97889e96b3d7de26b0ed28392841
    
    Change-Id: I210406162019593d7d177ff52950ce710a4104a6

--- hlte.mk
+++ hlte.mk
@@ -62,20 +62,17 @@ PRODUCT_PACKAGES += \
     libantradio
 
 # GPS
-PRODUCT_PACKAGES += \
-    gps.msm8974
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/flp.conf:/system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:/system/etc/sap.conf
 
 # camera
 PRODUCT_PACKAGES += \
     camera.msm8974 \
     libxml2 \
     Snap 
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/flp.conf:/system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf
 
 # Input device
 PRODUCT_COPY_FILES += \
