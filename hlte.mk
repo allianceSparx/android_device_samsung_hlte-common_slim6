555b57f41ee9a096a2fbcb105c99e96711b25abc
sparx <alliancesparx@gmail.com>
2017-10-06 10:51:46 +0100

    Build HIDL hals

--- hlte.mk
+++ hlte.mk
@@ -76,6 +76,27 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
+PRODUCT_PACKAGES += \
+   android.hardware.audio@2.0-impl \
+   android.hardware.audio.effect@2.0-impl \
+   android.hardware.bluetooth@1.0-impl \
+   android.hardware.drm@1.0-impl \
+   android.hardware.audio@2.0-impl \
+   android.hardware.audio.effect@2.0-impl \
+   android.hardware.graphics.allocator@2.0-impl \
+   android.hardware.graphics.allocator@2.0-service \
+   android.hardware.graphics.mapper@2.0-impl \
+   android.hardware.graphics.composer@2.1-impl \
+   android.hardware.graphics.memtrack@1.0-impl \
+   android.hardware.health@1.0-impl \
+   android.hardware.renderscript@1.0-impl \
+   android.hardware.gatekeeper@1.0-impl \
+   android.hardware.keymaster@3.0-impl \
+   android.hardware.light@2.0-impl \
+   android.hardware.nfc@1.0-impl \
+   android.hardware.power@1.0-impl \
+   android.hardware.sensors@1.0-impl 
+    
 # camera
 #PRODUCT_PACKAGES += \
     camera.msm8974 \
