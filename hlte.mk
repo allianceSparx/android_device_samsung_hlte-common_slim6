1d4f543f5833b10b192ba861a85130529a7c6fb6
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-15 15:32:46 +0100

    hlte-common: Add keymaster and gatekeeper HAL impl and service

--- hlte.mk
+++ hlte.mk
@@ -82,6 +82,15 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
@@ -101,7 +110,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
     
-# Power HAL
+# Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     power.msm8974
